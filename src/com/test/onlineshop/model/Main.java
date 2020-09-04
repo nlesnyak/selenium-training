bf2da26682248d6f0c268064a294a91fcde1f3b9
nlesniak <natalia.lesnyak@gmail.com>
2020-09-04 18:47:01 +0300

    Initial commit

---
+++ src/com/test/onlineshop/model/Main.java
@@ -0,0 +1,8 @@
+package com.test.onlineshop.model;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/java-example/src/test/java/ru/selenium/training/MyFirstTest.java
@@ -0,0 +1,52 @@
+package ru.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+/**
+ * Created by k.grigorchuk on 27.02.2017.
+ */
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+
+    @Before
+    public void start(){
+        //System.setProperty("webdriver.chrome.driver", "C:\\workspace\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+
+    @Test
+    public void MyFirstTest(){
+        driver.get("http://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("webdriver");
+        driver.findElement(By.name("btnK")).click();
+        System.out.println(driver.getTitle());
+        String text = "webdriver - Поиск в Google";
+        wait.until(titleIs(text));
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+
+}
