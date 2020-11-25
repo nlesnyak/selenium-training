4dc875aa8a310c9bfd0fa517a80872c0bf0353c8
nlesniak <natalia.lesnyak@gmail.com>
2020-11-25 22:51:58 +0300

    waits optimization

--- src/java-example/src/test/java/ru/selenium/training/CheckMenuItemsTest.java
+++ src/java-example/src/test/java/ru/selenium/training/CheckMenuItemsTest.java
@@ -1,35 +1,27 @@
 package ru.selenium.training;
 
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertNotNull;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class CheckMenuItemsTest {
     private WebDriver driver;
-    //= null;
     private WebDriverWait wait;
 
     @Before
     public void start() {
-//        driver = new ChromeDriver();
-//        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-//        driver.manage().timeouts();
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 2);
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
@@ -37,35 +29,34 @@ public class CheckMenuItemsTest {
 
         final String menuItemLocator = "//ul[@id='box-apps-menu']/li/a";
         final String subMenuItemLocator = "//ul[@id='box-apps-menu']/li/ul/li";
-        final String headerXpathValue = "//main[@id='main']/div[@id='content']//div[@class='panel-heading']";
+        //final String headerXpathValue = "//main[@id='main']/div[@id='content']//div[@class='panel-heading']";
+        final String headerXpathValue = "//div[@class='panel-heading']";
 
         driver.get("http://localhost:8080/litecart/admin/login.php");
-        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 
-        WebElement loginField = driver.findElement(By.name("username"));
+        WebElement loginField = wait.until(visibilityOfElementLocated(By.name("username")));
         loginField.clear();
         loginField.sendKeys("admin");
 
-        WebElement passwordField = driver.findElement(By.name("password"));
+        WebElement passwordField = wait.until(visibilityOfElementLocated(By.name("password")));
         passwordField.clear();
         passwordField.sendKeys("admin");
 
-        WebElement loginButton = driver.findElement(By.cssSelector("button"));
+        WebElement loginButton = wait.until(visibilityOfElementLocated(By.cssSelector("button")));
         loginButton.click();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        List<WebElement> menuItems = driver.findElements(By.xpath(menuItemLocator));
+        List<WebElement> menuItems = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(menuItemLocator)));
 
         for (int i = 0; i < menuItems.size(); i++) {
-            menuItems = driver.findElements(By.xpath(menuItemLocator));
+           menuItems = driver.findElements(By.xpath(menuItemLocator));
             menuItems.get(i).click();
-            assertTrue(driver.findElement(By.xpath(headerXpathValue)).isDisplayed());
+            assertNotNull(driver.findElement(By.xpath(headerXpathValue)).getText());
 
             List<WebElement> subMenuItems = driver.findElements(By.xpath(subMenuItemLocator));
             for (int j = 0; j < subMenuItems.size() ; j++) {
                 subMenuItems = driver.findElements(By.xpath(subMenuItemLocator));
                 subMenuItems.get(j).click();
-                assertTrue(driver.findElement(By.xpath(headerXpathValue)).isDisplayed());
+                assertNotNull(driver.findElement(By.xpath(headerXpathValue)).getText());
             }
         }
     }
