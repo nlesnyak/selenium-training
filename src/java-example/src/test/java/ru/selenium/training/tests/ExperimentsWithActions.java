547d315ca79408f4c10ff4d312f8028ca5632f28
nlesniak <natalia.lesnyak@gmail.com>
2021-02-17 18:13:10 +0300

    Actions tests, i.e.: dragAndDrop - refactoring

--- src/java-example/src/test/java/ru/selenium/training/tests/ExperimentsWithActions.java
+++ src/java-example/src/test/java/ru/selenium/training/tests/ExperimentsWithActions.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ExperimentsWithActions {
     private WebDriver driver = null;
@@ -27,45 +26,32 @@ public class ExperimentsWithActions {
 
     @Test
     public void moveElementsTest() {
-        final By wholeList1ResultLocator =  By.xpath("//ul[@id='sortable1']/li");
-        final By wholeList2ResultLocator =  By.xpath("//ul[@id='sortable2']/li");
-        final By list1Item2Locator = By.xpath("//ul[@id='sortable1']/li[2]");
-        final By list1Item4Locator = By.xpath("//ul[@id='sortable1']/li[4]");
-        final By list2Item3Locator = By.xpath("//ul[@id='sortable2']/li[3]");
-        final By list2Item4Locator = By.xpath("//ul[@id='sortable2']/li[4]");
+        final By list1Locator =  By.xpath("//ul[@id='sortable1']/li");
+        final By list2Locator =  By.xpath("//ul[@id='sortable2']/li");
 
         driver.navigate().to("https://jqueryui.com/resources/demos/sortable/connect-lists.html");
+
         Actions action = new Actions(driver);
-        WebElement item2 = driver.findElement(list1Item2Locator);
-        WebElement item4 = driver.findElement(list1Item4Locator);
-        action.dragAndDrop(item2, item4).perform();
+        List<WebElement> elementsList1 = driver.findElements(list1Locator);
+        action.dragAndDrop(elementsList1.get(1), elementsList1.get(3)).perform();
 
-        WebElement item22 = driver.findElement(list1Item2Locator);
-        WebElement item33 = driver.findElement(list2Item3Locator);
-        action.dragAndDrop(item22, item33).perform();
+        List<WebElement> updElementsList1 = driver.findElements(list1Locator);
+        List<WebElement> elementsList2 = driver.findElements(list2Locator);
+        action.dragAndDrop(updElementsList1.get(1), elementsList2.get(2)).perform();
 
-        String newItem2In1stList = driver.findElement(list1Item2Locator).getText();
-        String newItem3In2ndList = driver.findElement(list2Item3Locator).getText();
-        String newItem4In2ndList = driver.findElement(list2Item4Locator).getText();
-
-        Assert.assertTrue(newItem2In1stList.equals("Item 4"));
-        //System.out.println("1st List, 2nd item: " + newItem2In1stList);
-
-        Assert.assertTrue(newItem3In2ndList.equals("Item 3"));
-        //System.out.println("2nd list, 3rd item: " + newItem3In2ndList);
-
-        Assert.assertTrue(newItem4In2ndList.equals("Item 3"));
-        //System.out.println("2nd list, 4th item: " + newItem4In2ndList);
+        Assert.assertEquals("Item 4", driver.findElements(list1Locator).get(1).getText());
+        Assert.assertEquals("Item 3", driver.findElements(list2Locator).get(2).getText());
+        Assert.assertEquals("Item 3", driver.findElements(list2Locator).get(3).getText());
 
         List<String> list1 = new ArrayList();
         List<String> list2 = new ArrayList();
 
-        driver.findElements(wholeList1ResultLocator).stream().forEach(listResult1 -> list1.add(listResult1.getText()));
+        driver.findElements(list1Locator).forEach(listResult1 -> list1.add(listResult1.getText()));
         System.out.println("1st list: ");
         list1.forEach(System.out::println);
         System.out.println("");
 
-        driver.findElements(wholeList2ResultLocator).stream().forEach(listResult2 -> list2.add(listResult2.getText()));
+        driver.findElements(list2Locator).forEach(listResult2 -> list2.add(listResult2.getText()));
         System.out.println("2nd list: ");
         list2.forEach(System.out::println);
     }
